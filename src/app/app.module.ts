cf88c4204a2aa79b8715362608011fb2f3ea886b
Mila Tran <mila.tran@contemi.com>
2023-10-18 18:08:50 +0700

    9 - Component interaction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroParentComponent } from './hero-parent/hero-parent.component';
+import { HeroChildComponent } from './hero-child/hero-child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroParentComponent,
+    HeroChildComponent
   ],
   imports: [
     BrowserModule
